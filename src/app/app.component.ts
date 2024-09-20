042756c669442e088af7dedc449754ac95ee24b8
aadity tiwari <aaditytiwari3@gmail.com>
2024-09-21 00:57:10 +0530

    Added the navbar in menu formate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -15,10 +15,22 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit {
   title = 'biz-services-client-portal';
-  constructor(public clientService: ClientService,private router:Router){}
+  constructor(public clientService: ClientService,private router:Router,private elementRef: ElementRef){}
   isLogedIn = false;
   
   showLogoutButton = false;
+  isMenuVisible = false;
+  menuItems = [
+    { label: 'Property Deatils', link: '/' },
+    { label: 'Form 16B', link: '/form-16B' },
+    { label: 'TDS Compliance', link: '/tds-compliance' },
+    { label: 'Seller Payment', link: '/payments-to-seller' },
+  ];
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
   ngOnInit(): void {
       this.isLogedIn = this.clientService.isLogedIn();
 
@@ -37,6 +49,26 @@ export class AppComponent implements OnInit {
         }
       )
   }
+
+  navigateToPage(pageLink:any){
+    this.router.navigate([pageLink]);
+    this.toggleMenu();
+  }
+  handleClickOutside(event: Event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.isMenuVisible = false;
+    }
+  }
+  preventClick(){
+
+  }
+
+  // @HostListener('document:click') // Listen for clicks anywhere on the document
+  // onClick() {
+  //   if (this.isMenuVisible) { // Close only if menu is visible
+  //     this.isMenuVisible = false;
+  //   }
+  // }
 
   logout(){
     this.clientService.logout();
